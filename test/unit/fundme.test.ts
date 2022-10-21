6b7b70989e475331ff223d236071d782856ce610
samuel aniekan <samuelaniekan680@gmail.com>
2022-10-21 09:26:16 +0100

    testing

--- test/unit/fundme.test.ts
+++ test/unit/fundme.test.ts
@@ -1,3 +1,4 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { assert, expect } from "chai";
 import { deployments, ethers, getNamedAccounts } from "hardhat";
 import { FundMe, MockV3Aggregator } from "../../typechain-types";
@@ -7,10 +8,13 @@ describe("Fundme", function () {
     deployer: string,
     MockV3Aggregator: MockV3Aggregator,
     sendvalue = ethers.utils.parseEther("11");
-  console.log(sendvalue.toString());
+  let testAccount: string;
 
   beforeEach(async function () {
     deployer = (await getNamedAccounts()).deployer;
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    testAccount = otherAccount.address;
     await deployments.fixture(["all"]);
     Fundme = await ethers.getContract("FundMe", deployer);
     MockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer);
@@ -34,6 +38,23 @@ describe("Fundme", function () {
       await Fundme.fund({ value: sendvalue });
       const response = await Fundme.addressToAmountFunded(deployer);
       assert.equal(response.toString(), sendvalue.toString());
+    });
+  });
+
+  describe("withdraw", async () => {
+    beforeEach(async () => {
+      await Fundme.fund({ value: sendvalue });
+    });
+    it("only owner can withdraw", async () => {
+      const accounts = await ethers.getSigners();
+      for (let i = 1; i < 6; i++) {
+        const fundmeConnect = Fundme.connect(accounts[i]);
+
+        await expect(fundmeConnect.withdraw()).to.be.revertedWithCustomError(
+          Fundme,
+          "FundMe_NotOwner"
+        );
+      }
     });
   });
 });
